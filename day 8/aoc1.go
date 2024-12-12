6536d89634186c8b56c2ed1a9a27ab63fa85613b
FlashGrey3000 <aaftabkhan2005@gmail.com>
2024-12-12 23:26:20 +0530

    part 2 try 1, ans too low?

--- day 8/aoc1.go
+++ day 8/aoc1.go
@@ -23,6 +23,7 @@ func main() {
 	}
 
 	fmt.Println("antinodes: ", calcAntiNodes(grid))
+	fmt.Println("Antinodes after considering resonance: ", ResonantAntiNodes(grid))
 }
 
 func calcAntiNodes(grid map[byte][]point) (int) {
@@ -58,4 +59,33 @@ func overlapping(x, y int, visited []point) (bool) {
 		}
 	}
 	return false
+}
+
+func ResonantAntiNodes(grid map[byte][]point) (int) {
+	count := 0
+	visited := make([]point, 0)
+	for k,v := range grid {
+		if k == '.' {
+			continue
+		}
+		for i:=0; i<len(v); i++ {
+			for j:=0; j<len(v); j++ {
+				if i==j {
+					continue
+				}
+				for z:=1; ; z++ {
+					x:=v[j].X + z*(v[j].X - v[i].X)
+					y:=v[j].Y + z*(v[j].Y - v[i].Y)
+					if x >= 0 && x < 50 && y >= 0 && y < 50 && !overlapping(x,y, visited){
+						count++
+						visited = append(visited, point{X: x, Y: y})
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+
+	return count
 }
