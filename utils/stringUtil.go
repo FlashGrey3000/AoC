9d7d66b1ee26e16f2668bba6b4d980420af7706f
FlashGrey3000 <aaftabkhan2005@gmail.com>
2024-12-12 19:25:33 +0530

    added more utils

--- utils/stringUtil.go
+++ utils/stringUtil.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,4 +13,20 @@ func readLines(filename string) []string {
 	}
 
 	return strings.Split(string(file), "\r\n")
+}
+
+func Atoi(a string) (int) {
+	x,err := strconv.Atoi(a)
+	if err != nil {
+		return -99999999
+	}
+	return x
+}
+
+func AvectoIvec(aVec string) ([]int) {
+	iVec := make([]int, len(aVec))
+	for i,a := range aVec {
+		iVec[i] = Atoi(string(a))
+	}
+	return iVec
 }
